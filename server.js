ddb89b90c9a8398cdcff0d57b4582ae2535708a6
Greg Lee <gml2136@gmail.com>
2017-05-09 00:16:20 -0700

    added a few packages, and also got rid of the force true

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ require('./config/strategy.js')(passport, LocalStrategy);
 // var usersRoutes = require('./server/routes/usersRoutes')(app, express, passport);
 // app.use('/users', usersRoutes);
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
