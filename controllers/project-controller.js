88d62ab178ab842e0f04c8f8ad52c16798f195b5
Greg Lee <gml2136@gmail.com>
2017-05-10 19:24:18 -0700

    greg route changes

---
+++ controllers/project-controller.js
@@ -0,0 +1,17 @@
+module.exports = function(router, db) {
+
+  router.get('/project/browse', function(req, res) {
+    db.Project.findAll();
+  });
+
+  router.get("/project/:id", function(req, res) {
+    db.Project.FindOne()
+  });
+
+
+  router.post("/project/:id", function(req, res) {
+    let projID = req.params.id;
+    let invID = req.body.id;
+    db.InvestorInvite.create();
+  });
+}
